ec34d700b1cec00f7120bdf4c4cad025e71e06ee
IvanTsapkoff <113112611+IvanTsapkoff@users.noreply.github.com>
2022-11-20 03:08:57 +0300

    Add files via upload

---
+++ lab_4_4.py
@@ -0,0 +1,15 @@
+gloss = {
+	'sergey': 18,
+	'lera': 8,
+	'vika': 15,
+	'alisa': 4,
+	'denis': 18,
+	'dasha': 17	
+	}
+	
+def func_3(g,n):
+	print('Введите название словаря и ключь в кавычках')
+	return(print(g[n]))
+	
+func_3(gloss,'vika')
+	
